f9f076328d80c96d6ee43122754269d20f0f3b00
RyanNguyen0203 <ryannguyen0203@gmail.com>
2017-07-02 10:07:48 +0700

    style: fix formatting issues according to PEP8 online checker http://pep8online.com

--- classes.py
+++ classes.py
@@ -1,9 +1,13 @@
 import webbrowser
 
+
 class Movie(object):
-	""" A class for movie-related information and methods """
-	def __init__(self,title,storyline,poster_url,trailer_url):
-		self.title = title
-		self.storyline = storyline 
-		self.poster_url = poster_url
-		self.trailer_url = trailer_url
+    """ A class for movie-related information and methods """
+
+    def __init__(self, title, storyline, poster_url, trailer_url):
+        ''' Initialize Movie instance with instance variables:
+        title, storyline, poster_url, trailer_url'''
+        self.title = title
+        self.storyline = storyline
+        self.poster_url = poster_url
+        self.trailer_url = trailer_url
--- create_web_page.py
+++ create_web_page.py
@@ -1,15 +1,63 @@
-import classes, functions
+import classes
+import functions
 
-# Initialize Movie instances 
-batman_begins = classes.Movie('Batman Begins','A rich dude in his bat suite','https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg','https://www.youtube.com/watch?v=qHhHIbNuok8')
-the_lord_of_the_rings = classes.Movie('The Lord of the Rings','An unforgiving war between the Good and the Bad','https://upload.wikimedia.org/wikipedia/en/8/87/Ringstrilogyposter.jpg','https://www.youtube.com/watch?v=r5X-hFf6Bwo')
-a_good_year = classes.Movie('A Good Year', 'A rich banker found his true love', 'https://upload.wikimedia.org/wikipedia/en/4/47/A_Good_Year.jpg', 'https://www.youtube.com/watch?v=-vxmYKvN8fo')
-interstellar = classes.Movie('Interstellar','An interstellar mission for the future of humanity','https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg','https://www.youtube.com/watch?v=zSWdZVtXT7E')
-high_school_musical_3 = classes.Movie('High School Musical 3','A bunch of high school kids in their senior year','https://upload.wikimedia.org/wikipedia/en/a/af/HSM_3_Poster.JPG','https://www.youtube.com/watch?v=bEQXcbqvbT0')
-dark_water = classes.Movie('Dark Water','A single mom and the ghost in her apartment','https://upload.wikimedia.org/wikipedia/en/4/41/Darkwaterposter.jpg','https://www.youtube.com/watch?v=JcpJVEp047Q')
+# Initialize Movie instances
+
+''' Batman Begins movie: movie title,
+storyline, poster image url and movie trailer url'''
+batman_begins = classes.Movie(
+    'Batman Begins',
+    'A rich dude in his bat suite',
+    'https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg',
+    'https://www.youtube.com/watch?v=qHhHIbNuok8')
+
+'''The Lord of the Rings movie: movie title,
+storyline, poster image url and movie trailer url'''
+the_lord_of_the_rings = classes.Movie(
+    'The Lord of the Rings',
+    'An unforgiving war between the Good and the Bad',
+    'https://upload.wikimedia.org/wikipedia/en/8/87/Ringstrilogyposter.jpg',
+    'https://www.youtube.com/watch?v=r5X-hFf6Bwo')
+
+'''A Good Year movie: movie title,
+storyline, poster image url and movie trailer url'''
+a_good_year = classes.Movie(
+    'A Good Year',
+    'A rich banker found his true love',
+    'https://upload.wikimedia.org/wikipedia/en/4/47/A_Good_Year.jpg',
+    'https://www.youtube.com/watch?v=-vxmYKvN8fo')
+
+'''Interstellar movie: movie title,
+storyline, poster image url and movie trailer url'''
+interstellar = classes.Movie(
+    'Interstellar',
+    'An interstellar mission for the future of humanity',
+    'https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg',
+    'https://www.youtube.com/watch?v=zSWdZVtXT7E')
+
+'''High School Musical 3 movie: movie title,
+storyline, poster image url and movie trailer url'''
+high_school_musical_3 = classes.Movie(
+    'High School Musical 3',
+    'A bunch of high school kids in their senior year',
+    'https://upload.wikimedia.org/wikipedia/en/a/af/HSM_3_Poster.JPG',
+    'https://www.youtube.com/watch?v=bEQXcbqvbT0')
+
+'''Dark Water movie: movie title, storyline,
+poster image url and movie trailer url'''
+dark_water = classes.Movie(
+    'Dark Water',
+    'A single mom and the ghost in her apartment',
+    'https://upload.wikimedia.org/wikipedia/en/4/41/Darkwaterposter.jpg',
+    'https://www.youtube.com/watch?v=JcpJVEp047Q')
 
 # Create a list of Movie instances
-list_of_movies = [batman_begins, the_lord_of_the_rings, a_good_year, interstellar, high_school_musical_3,dark_water]
+list_of_movies = [batman_begins,
+                  the_lord_of_the_rings,
+                  a_good_year,
+                  interstellar,
+                  high_school_musical_3,
+                  dark_water]
 
-# Create the movie page 
-functions.open_movies_page(list_of_movies) 
+# Create the movie page
+functions.open_movies_page(list_of_movies)
